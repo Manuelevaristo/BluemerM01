e0437191ec0381941c35c854b6d6eab3b5149096
Manuelevaristo <65638655+Manuelevaristo@users.noreply.github.com>
2021-08-18 20:06:44 -0300

    Add files via upload

---
+++ index (1).js
@@ -0,0 +1,9 @@
+console.log("Cadastro de Clientes");
+console.log("0 – Fim");
+console.log("1 - Inclui");
+console.log("2 - Altera");
+console.log("3 - Exclui");
+console.log("4 - Consulta");
+const opcao=+prompt("Escolhe uma opção: ")
+
+console.log(`Você escolheu a opção ${opcao}`)
---
+++ index.js
@@ -0,0 +1,7 @@
+//console.log('Olá Mundo!');
+//prompt("Digite seu nome: ");
+
+//let usuario = 'Evaristo';
+//console.log(usuario);
+let nome=prompt("Digite teu nome: ");
+console.log('Olá '+nome+', seja Bem-vindo');
